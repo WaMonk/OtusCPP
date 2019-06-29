fa8832d8cd9f3c7609294cb914664aff94016376
WaMonk <besshiro@gmail.com>
2019-06-29 18:30:51 +0300

    work to prepare script

--- ci/travis_prepare.sh
+++ ci/travis_prepare.sh
@@ -1,8 +1,15 @@
+CWD=$PWD
+
+echo "Install Modules: "
 sudo apt-get install libboost-test-dev libgtest-dev build-essential -y
+
+echo "Configure GTest: "
 cd /usr/src/gtest
 cmake .
 cmake --build . --target install
 sudo mv libgtest* /usr/lib/
-cd "${TRAVIS_BUILD_DIR}"
+cd $CWD
+
+echo "Deb update: "
 echo "deb http://archive.ubuntu.com/ubuntu xenial main universe" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -qq
