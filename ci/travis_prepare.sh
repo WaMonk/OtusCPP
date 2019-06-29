f6e59a372d5e40678a73c41ad0c8ea07e5759d96
WaMonk <besshiro@gmail.com>
2019-06-29 19:16:30 +0300

    work on travis prepare script #9

--- ci/travis_prepare.sh
+++ ci/travis_prepare.sh
@@ -11,8 +11,8 @@ sudo apt-get install libboost-test-dev build-essential libgtest-dev cmake
 echo "Configure GTest: "
 cd /usr/src/gtest
 sudo cmake .
-sudo cmake --build . --target install
-sudo mv libgtest* /usr/lib/
+sudo cmake --build .
+sudo cp *.a /usr/lib/
 cd $CWD
 
 echo "Finish."
