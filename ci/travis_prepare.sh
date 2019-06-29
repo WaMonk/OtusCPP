51596c8b64c9a9e05a3aea16744d30bcce6c18a2
WaMonk <besshiro@gmail.com>
2019-06-29 18:34:49 +0300

    work on travis prepare script

--- ci/travis_prepare.sh
+++ ci/travis_prepare.sh
@@ -1,15 +1,15 @@
 CWD=$PWD
-
+#
 echo "Install Modules: "
-sudo apt-get install libboost-test-dev libgtest-dev build-essential -y
-
+sudo apt-get install libboost-test-dev libgtest-dev build-essential
+#
 echo "Configure GTest: "
 cd /usr/src/gtest
 cmake .
 cmake --build . --target install
 sudo mv libgtest* /usr/lib/
 cd $CWD
-
+#
 echo "Deb update: "
 echo "deb http://archive.ubuntu.com/ubuntu xenial main universe" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update -qq
